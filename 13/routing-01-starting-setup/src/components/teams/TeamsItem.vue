8b0e0600b78aa328c4aba16243ae31ff32b86796
remigiuszd <remigiusz.j.dudek@metrosoft.com>
2025-01-22 08:23:03 +0100

    lesson 13 - use router-link instead of a

--- 13/routing-01-starting-setup/src/components/teams/TeamsItem.vue
+++ 13/routing-01-starting-setup/src/components/teams/TeamsItem.vue
@@ -2,7 +2,7 @@
   <li>
     <h3>{{ name }}</h3>
     <div class="team-members">{{ memberCount }} Members</div>
-    <a :href="`/teams/${this.id}`">View Members</a>
+    <router-link :to="`/teams/${this.id}`">View Members</router-link>
   </li>
 </template>
 
