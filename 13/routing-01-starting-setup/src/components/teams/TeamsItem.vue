cd16e76d86f0c68ff86f6cde84d246e3b2f4e6ea
remigiuszd <remigiusz.j.dudek@metrosoft.com>
2025-01-22 08:12:19 +0100

    lesson 13 - add dynamic route and go there programatically

--- 13/routing-01-starting-setup/src/components/teams/TeamsItem.vue
+++ 13/routing-01-starting-setup/src/components/teams/TeamsItem.vue
@@ -2,13 +2,13 @@
   <li>
     <h3>{{ name }}</h3>
     <div class="team-members">{{ memberCount }} Members</div>
-    <a href="#">View Members</a>
+    <a :href="`/teams/${this.id}`">View Members</a>
   </li>
 </template>
 
 <script>
 export default {
-  props: ['name', 'memberCount'],
+  props: ['id', 'name', 'memberCount'],
 };
 </script>
 
