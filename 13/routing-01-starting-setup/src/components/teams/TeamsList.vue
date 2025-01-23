f5526b963bd253b51b6e9cc59946cf907841144d
remigiuszd <remigiusz.j.dudek@metrosoft.com>
2025-01-23 05:37:41 +0100

    lesson 13 - define nested routes with key

--- 13/routing-01-starting-setup/src/components/teams/TeamsList.vue
+++ 13/routing-01-starting-setup/src/components/teams/TeamsList.vue
@@ -8,7 +8,10 @@
       :member-count="team.members.length"
     ></teams-item>
   </ul>
+  <router-view :key="this.$route.params.teamId"></router-view>
 </template>
+// router-view key is used to force a re-render of the component when the route changes
+// by default vue will reuse the component, hence we need to let vue know that it is a different component
 
 <script>
 import TeamsItem from './TeamsItem.vue';
