c000335d67fabea965d76da338c8925b2e9ce237
remigiuszd <remigiusz.j.dudek@metrosoft.com>
2025-01-23 06:31:11 +0100

    lesson 13 - multiple components for one page

--- 13/routing-01-starting-setup/src/main.js
+++ 13/routing-01-starting-setup/src/main.js
@@ -5,17 +5,19 @@ import App from './App.vue';
 import TeamsList from "@/components/teams/TeamsList.vue";
 import UsersList from "@/components/users/UsersList.vue";
 import TeamMembers from "@/components/teams/TeamMembers.vue";
+import TeamsFooter from "@/components/teams/TeamsFooter.vue";
+import UserFooter from "@/components/users/UserFooter.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/teams' },
         {
-            path: '/teams', component: TeamsList, children: [
+            path: '/teams', components: { default: TeamsList, footer: TeamsFooter }, children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props: true }
             ]
         },
-        { path: '/users', component: UsersList },
+        { path: '/users', components: { default: UsersList, footer: UserFooter } },
         { path: '/:notFound(.*)', redirect: '/teams' }
     ]
 })
