696ed4784dc8332e5eb80b3a139ce58f3db67bae
remigiuszd <remigiusz.j.dudek@metrosoft.com>
2025-01-27 08:47:09 +0100

    lesson 15 - challenge - move login state to vuex store

--- 15/vuex-11-a-challenge-starting-code/src/store/index.js
+++ 15/vuex-11-a-challenge-starting-code/src/store/index.js
@@ -11,7 +11,24 @@ const store = createStore({
         return {
             isLoggedIn: false
         }
+    },
+    mutations: {
+        login(state) {
+            state.isLoggedIn = true;
+        },
+        logout(state) {
+            state.isLoggedIn = false;
+        },
+    },
+    actions: {
+        login({ commit }) {
+            commit('login')
+        },
+        logout({ commit }) {
+            commit('logout');
+        },
     }
+
 })
 
 export default store;
