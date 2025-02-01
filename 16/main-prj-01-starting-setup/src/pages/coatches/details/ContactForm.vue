d0d823cd865338a2155844d7c0b8de90a0e4f9db
remigiuszd <remigiusz.j.dudek@metrosoft.com>
2025-02-01 05:38:35 +0100

    lesson 16 - main project - add styling to contact form

--- 16/main-prj-01-starting-setup/src/pages/coatches/details/ContactForm.vue
+++ 16/main-prj-01-starting-setup/src/pages/coatches/details/ContactForm.vue
@@ -1,5 +1,8 @@
 <script>
+import BaseButton from "@/pages/ui/BaseButton.vue";
+
 export default {
+  components: { BaseButton },
   props: ['id'],
   data() {
     return {
@@ -38,24 +41,64 @@ export default {
 <template>
   <h2>Contact form</h2>
   <form @submit.prevent="sendMessage">
-    <div>
+    <div class="form-control">
       <label for="to">Coach</label>
       <input type="text" id="to" :value="coachName" readonly disabled>
     </div>
-    <div>
+    <div class="form-control">
       <label for="email">Your email</label>
       <input type="email" id="email" required v-model="email">
     </div>
-    <div>
+    <div class="form-control">
       <label for="message">Your message</label>
       <textarea id="message" rows="5" required v-model="message"></textarea>
     </div>
-    <div>
-      <button>Send message</button>
+    <div class="actions">
+      <base-button mode="outline">Send message</base-button>
     </div>
   </form>
 </template>
 
 <style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
 
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
 </style>
